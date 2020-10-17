e9a6d8ab94a6feddb0a4cf9add220eb9a6f0a80c
superoft1 <superoft1@users.noreply.github.com>
2020-10-17 13:30:42 +0700

    Updated

--- FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
+++ FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+////////////
+using System.Data;
+using System.Data.SqlClient;
+using FoodRecipeApp.Classes;
+
 namespace FoodRecipeApp
 {
     /// <summary>
@@ -23,6 +28,34 @@ namespace FoodRecipeApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+		private void BtnOpenMenu_Click(object sender, RoutedEventArgs e)
+		{
+            btnCloseMenu.Visibility = Visibility.Visible;
+            btnOpenMenu.Visibility = Visibility.Hidden;
+        }
+
+		private void BtnCloseMenu_Click(object sender, RoutedEventArgs e)
+		{
+            btnOpenMenu.Visibility = Visibility.Visible;
+            btnCloseMenu.Visibility = Visibility.Hidden;
+		}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SQL_DB.openConnection();
+
+            /*SQL_DB.sql = "SELECT [ID], [Data], [Image] FROM RecipeInfo";
+            SQL_DB.cmd.CommandType = CommandType.Text;
+            SQL_DB.cmd.CommandText = SQL_DB.sql;
+            SQL_DB.da = new SqlDataAdapter(SQL_DB.cmd);
+            SQL_DB.dt = new DataTable();
+            SQL_DB.da.Fill(SQL_DB.dt);
+
+            SQL_Demo.ItemsSource = SQL_DB.dt.DefaultView;*/
+
+            SQL_DB.closeConnection();
         }
     }
 }
