852550138f69e36460bb06ad6386be2f64054645
Phu Do <dophu1402@gmail.com>
2020-10-20 12:47:01 +0700

    absolute icon_path menu

--- FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
+++ FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
@@ -35,6 +35,29 @@ namespace FoodRecipeApp
             
         }
 
+        class Menu : INotifyPropertyChanged
+        {
+            public string Content { get; set; }
+            public string Icon { get; set; }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        class MenuDao
+        {
+            public static BindingList<Menu> GetAll()
+            {
+                var list = new BindingList<Menu>()
+                {
+                    new Menu() { Content = "Home", Icon = "Resources/Icons/home.png" },
+                    new Menu() { Content = "Search", Icon = "Resources/Icons/search.png" },
+                    new Menu() { Content = "History", Icon = "Resources/Icons/history.png" },
+                    new Menu() { Content = "Settings", Icon = "Resources/Icons/settings.png" },
+                };
+                return list;
+
+            }
+        }
+        
         //drag window
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -59,9 +82,11 @@ namespace FoodRecipeApp
 
             StateClosed = !StateClosed;
         }
-
+        private BindingList<Menu> _list_menu;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            _list_menu = MenuDao.GetAll();
+            menuList.ItemsSource = _list_menu;
             SQL_DB.openConnection();
 
 			/*SQL_DB.sql = "SELECT [ID], [Data], [Image] FROM RecipeInfo";
